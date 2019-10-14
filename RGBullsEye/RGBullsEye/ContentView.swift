1a478d2ba90791c1552099205657a9d56be7af74
김지나 <jinagold.k@gmail.com>
2019-10-14 20:30:38 +0900

    Add rayWenderlich SwiftUI tutorials

---
+++ RGBullsEye/RGBullsEye/ContentView.swift
@@ -0,0 +1,59 @@
+//
+//  ContentView.swift
+//  RGBullsEye
+//
+//  Created by 김지나 on 08/10/2019.
+//  Copyright © 2019 김지나. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let rTarget = Double.random(in: 0..<1)
+    let gTarget = Double.random(in: 0..<1)
+    let bTarget = Double.random(in: 0..<1)
+    @State var rGuess: Double
+    @State var gGuess: Double
+    @State var bGuess: Double
+    
+    var body: some View {
+        VStack {
+            HStack {
+                VStack {
+                    Color(red: rTarget, green: gTarget, blue: bTarget)
+                    Text("Match this color")
+                }
+                VStack {
+                    Color(red: rGuess, green: gGuess, blue: bGuess)
+                    Text("R: \(Int(rGuess * 255.0))"
+                    + "  G: \(Int(gGuess * 255.0))"
+                    + "  B: \(Int(bGuess * 255.0))")
+                }
+            }
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                Text("Hit Me!")
+            }
+            ColorSlider(value: $rGuess, textColor: .red)
+            ColorSlider(value: $gGuess, textColor: .green)
+            ColorSlider(value: $bGuess, textColor: .blue)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView(rGuess: 0.5, gGuess: 0.3, bGuess: 0.7).previewLayout(.fixed(width: 568, height: 320))
+    }
+}
+
+struct ColorSlider: View {
+    @Binding var value: Double
+    var textColor: Color
+    var body: some View {
+        HStack {
+            Text("0").foregroundColor(textColor)
+            Slider(value: $value)
+            Text("255").foregroundColor(textColor)
+        }.padding(.horizontal)
+    }
+}
