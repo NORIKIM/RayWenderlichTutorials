219e0f9bf7ef4bf7513ece2ffb8ba3338b24c7c0
김지나 <jinagold.k@gmail.com>
2019-11-26 13:35:23 +0900

    Add sample and images

---
+++ adaptiveLayout/adaptiveLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  adaptiveLayout
+//
+//  Created by 김지나 on 2019/11/26.
+//  Copyright © 2019 김지나. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
